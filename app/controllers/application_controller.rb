976a0b4610fb8257dbe7cefccf19484ed54d702b
AMOL <madnea.v@gmail.com>
2017-07-09 12:14:30 +0530

    user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,7 +58,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :first_name, :last_name, :mobile_number, :role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :username, :first_name, :last_name, :mobile_number, :role_id, :branch_id])
   end
 
 end
